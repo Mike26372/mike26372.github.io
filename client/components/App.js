e323d78c26e950867064fe47f3a95ad9bf6480bb
Michael Tutt <tutt.michael@gmail.com>
2017-06-22 15:11:14 -0700

    add client components

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+import Splash from './Splash/Splash.js';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <Splash />
+    )
+  }
+}
+
+export default App;
---
+++ client/components/Splash/Splash.js
@@ -0,0 +1,16 @@
+import { Component } from 'react'; 
+import styles from './Splash.css';
+
+class Splash extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>Splash module</div>
+    );
+  }
+}
+
+export default Splash;
