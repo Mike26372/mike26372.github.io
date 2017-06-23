e203777795e9dfb175dafa2204fdd63daba4e046
Michael Tutt <tutt.michael@gmail.com>
2017-06-22 18:00:18 -0700

    add content component for personal information

---
+++ client/components/Content/Content.js
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+
+import styles from './Content.css';
+
+class Content extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="content-wrapper">
+        <div className="content">
+          <h2 className="content-head is-center">Excepteur sint occaecat cupidatat.</h2>
+
+          <div className="pure-g">
+            <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+
+                <h3 className="content-subhead">
+                    <i className="fa fa-rocket"></i>
+                    Get Started Quickly
+                </h3>
+                <p>
+                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                </p>
+            </div>
+            <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 className="content-subhead">
+                    <i className="fa fa-mobile"></i>
+                    Responsive Layouts
+                </h3>
+                <p>
+                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                </p>
+            </div>
+            <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 className="content-subhead">
+                    <i className="fa fa-th-large"></i>
+                    Modular
+                </h3>
+                <p>
+                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                </p>
+            </div>
+            <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 className="content-subhead">
+                    <i className="fa fa-check-square-o"></i>
+                    Plays Nice
+                </h3>
+                <p>
+                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Content;
