b3f312db3a47e2ca6bb21593e6fb4fe77335a081
Michael Tutt <tutt.michael@gmail.com>
2017-06-22 17:59:52 -0700

    Add main App component and give root css variables

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,37 @@
 import { Component } from 'react';
 
+import styles from './App.css';
+
+import Nav from './Nav/Nav.js';
 import Splash from './Splash/Splash.js';
+import Content from './Content/Content.js';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
+    this.handleScroll = this.handleScroll.bind(this);
   }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(e) {
+    let { scrollTop } = event.target.body;
+    console.log(scrollTop);
+  }
+
   render() {
     return (
-      <Splash />
+      <div>
+        <Nav />
+        <Splash />
+        <Content />
+      </div>
     )
   }
 }
