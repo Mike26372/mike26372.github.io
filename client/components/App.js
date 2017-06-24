31bc99e33f890674e6cd2e7a2a8209694bde8134
Michael Tutt <tutt.michael@gmail.com>
2017-06-23 21:57:16 -0700

    use transition group plus for controlled transitions

--- client/components/App.js
+++ client/components/App.js
@@ -1,36 +1,82 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import TransitionGroupPlus from 'react-transition-group-plus';
 
 import styles from './App.css';
 
 import Nav from './Nav/Nav.js';
 import Splash from './Splash/Splash.js';
 import Content from './Content/Content.js';
+import About from './About/About.js';
+
+var windows = [
+  <Splash key={0}/>,
+  <About key={1}/>
+]
 
 class App extends Component {
   constructor() {
     super();
+    
+    this.state = {
+      show: false,
+      index: 0,
+      height: null,
+      width: null,
+      sections: 2
+    };
+
     this.handleScroll = this.handleScroll.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+    this.toggleShow = this.toggleShow.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('resize', this.handleResize);
+    // set scrollTop to zero? Moves back to top of page
+    // window.scrollTo(0, 0);
+    let { innerHeight, innerWidth } = window;
+    this.setState({ height: innerHeight, width: innerWidth });
+  }
+
+  componentDidUpdate() {
+    window.scrollTop = 0;
   }
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('resize', this.handleResize);
   }
 
   handleScroll(e) {
     let { scrollTop } = event.target.body;
-    console.log(scrollTop);
+    let { sections, height, width, index } = this.state;
+    let totalHeight = sections * height;
+    let newIndex = Math.floor((scrollTop / totalHeight) * sections);
+    if (newIndex !== index) {
+      this.setState({ index: newIndex }); 
+    }
+
+  }
+
+  handleResize(e) {
+    let { innerWidth, innerHeight } = window;
+    this.setState( {height: innerHeight, width: innerWidth });
+  }
+
+  toggleShow(e) {
+    this.setState({ show: true })
   }
 
   render() {
+    let { show, index } = this.state;
     return (
       <div>
-        <Nav />
-        <Splash />
-        <Content />
+        <Nav show={show} toggleShow={this.toggleShow}/>
+        <TransitionGroupPlus transitionMode="out-in">
+          {show ? windows[index] : ''}
+        </TransitionGroupPlus>
       </div>
     )
   }
