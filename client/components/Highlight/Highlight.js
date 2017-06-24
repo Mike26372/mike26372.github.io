f2f1e8b0dfb1d249351e4955f4c7314dcc081c7e
Michael Tutt <tutt.michael@gmail.com>
2017-06-23 21:59:11 -0700

    component for main description

---
+++ client/components/Highlight/Highlight.js
@@ -0,0 +1,21 @@
+import styles from './Highlight.css';
+
+const Highlight = (props) => {
+  let { title, text, icon } = props;
+  let highlightRow = `pure-u-1 pure-u-md-1-3 ${styles.row}`;
+
+
+  return (
+    <div className={ highlightRow }>
+      <h3 className={ styles.title }>
+        { title }
+      </h3>
+      <p className={ styles.text }>
+        { text }
+      </p>
+      <span></span>
+    </div>
+  )
+}
+
+export default Highlight;
---
+++ client/components/Highlight/highlights.js
@@ -0,0 +1,7 @@
+var highlights = [
+  {title: "Optimization Obsession", text: "Concise, semantic code is a way of life, and modern front-end frameworks / best-practices make life that worth living", icon: null},
+  {title: "Creative Problem Solver", text: "Love for discovering new tools used to conquer unique problems, and I'll work tirelessly to solve them", icon: null},
+  {title: "Knack For Design", text: "Utilizing prime organization skills and substantial attention to detail, I strive to make sure every pixel is in order", icon: null},
+];
+
+export default highlights; 
