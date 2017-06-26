6fa3c96bf88e745abb1235b771efa49997311742
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 15:35:47 -0700

    add Tech container

---
+++ client/components/Tech/Tech.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { TweenMax } from 'gsap';
+
+import styles from './Tech.css';
+
+class Tech extends Component {
+  constructor() {
+    super();
+  }
+
+  componentWillEnter(cb) {
+    const el = this.container;
+    TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: cb});
+  }
+
+  componentWillLeave(cb) {
+    const el = this.container;
+    TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0, onComplete: cb});
+  }
+
+  render() {
+
+    let rowsStyle = `pure-g ${styles.rows}`;
+    let rowStyle = `pure-u-1 ${styles.row}`;
+    let titleStyle = `${styles.largeText}`;
+
+    return (
+      <div className={styles.container} ref={ c => this.container = c }>
+        <div className={styles.content}>
+          <div className={rowsStyle}>
+            <div className={rowStyle}>
+              <h2 className={titleStyle}>tech I enjoy using...</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tech;
