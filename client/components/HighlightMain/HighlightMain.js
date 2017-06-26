1f19b8e2e9235d7c4f2e6b78d55dcaa13aad7dcf
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 10:06:47 -0700

    update main hightlight for inline svg

--- client/components/HighlightMain/HighlightMain.js
+++ client/components/HighlightMain/HighlightMain.js
@@ -1,20 +1,30 @@
 import styles from './HighlightMain.css';
 
+import ComputerSVG from '../ComputerSVG/ComputerSVG.js';
+
+
 const HighlightMain = (props) => {
 
-  let highlightMainRow = `pure-u-1 ${styles.row}`;
-
+  let row1Column = `pure-u-1 ${styles.row}`;
+  let row2Column = `pure-u-1-1 ${styles.row}`;
 
   return (
-    <div className={ highlightMainRow }>
-      <p className={ styles.largeText }>Hello, my name is Michael.</p>
-      <p className={ styles.smallText }>I'm a full-stack software engineer living in Northern California. </p>
-      <p className={ styles.smallText }>
-        I develop web applications with significant 
-        experience in both the front and back-end, 
-        specializing in meticulously crafted user experiences.
-      </p>
-      <p></p>
+    <div>    
+      <div className={ row2Column }>
+        <h2 className={ styles.largeText }>a bit about me...</h2>
+      </div>
+      <div className={ row2Column }>
+        <ComputerSVG />
+      </div>
+      <div className={ row1Column }>
+        <p className={ styles.smallText }>I'm Michael Tutt, a full-stack software engineer living in Northern California. </p>
+        <p className={ styles.smallText }>
+          I develop web applications with significant 
+          experience in both the front and back-end, 
+          specializing in meticulously crafted user experiences.
+        </p>
+        <p></p>
+      </div>
     </div>
   )
 }
