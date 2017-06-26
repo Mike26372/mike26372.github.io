1610dde610612bf575d71529c04396d60f29fb3c
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 10:08:43 -0700

    move highlights into sub highlight component

---
+++ client/components/HighlightSub/HighlightSub.js
@@ -0,0 +1,21 @@
+import styles from './HighlightSub.css';
+
+import highlights from '../Highlight/highlights.js';
+import Highlight from '../Highlight/Highlight.js';
+
+
+const HighlightSub = (props) => {
+
+  let titleStyle = `pure-u-1`;
+
+  return (
+    <div>
+      <div className={titleStyle}> 
+        <h2 className={styles.largeText}>some highlights...</h2>
+      </div>
+      {highlights.map(hl => <Highlight title={hl.title} text={hl.text} icon={hl.icon} />)}
+    </div>
+  )
+}
+
+export default HighlightSub;
