b305b544f7d08e1b2b65426c4ff57aabf9e62eb1
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 15:34:32 -0700

    map svgs to highlights

---
+++ client/components/Highlight/svgMapping.js
@@ -0,0 +1,15 @@
+
+import Lightbulb from '../LightbulbSVG/LightbulbSVG.js';
+import Computer from '../ComputerSVG/ComputerSVG.js';
+import Design from '../DesignSVG/DesignSVG.js';
+import Spedometer from '../SpedometerSVG/SpedometerSVG.js';
+
+
+const svgMapping = {
+  'Computer': Computer,
+  'Spedometer': Spedometer,
+  'Design': Design,
+  'Lightbulb': Lightbulb,
+};
+
+export default svgMapping;
