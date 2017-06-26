6aad36df55f7bf60b22d15d2392150c71bd12b95
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 15:35:55 -0700

    Add traits container

---
+++ client/components/Traits/Traits.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { TweenMax } from 'gsap';
+
+import styles from './Traits.css';
+import HighlightSub from '../HighlightSub/HighlightSub.js';
+
+class Traits extends Component {
+  constructor() {
+    super();
+  }
+
+  componentWillEnter(cb) {
+    const el = this.container;
+    TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: cb});
+  }
+
+  componentWillLeave(cb) {
+    const el = this.container;
+    TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0, onComplete: cb});
+  }
+
+  render() {
+
+    let rowsStyle = `pure-g ${styles.rows}`;
+
+    return (
+      <div className={styles.container} ref={ c => this.container = c }>
+        <div className={styles.content}>
+          <div className={rowsStyle}>
+            <HighlightSub />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Traits;
