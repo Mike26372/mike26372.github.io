f9b8dd0650a3de4566ade05f9aa66878d3938d43
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 10:07:30 -0700

    update splash to normal positioning instead of absolute

--- client/components/Splash/Splash.js
+++ client/components/Splash/Splash.js
@@ -7,21 +7,34 @@ class Splash extends Component {
     super();
   }
 
-  componentWillEnter(cb) {
-    const el = this.container;
-    TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: cb});
-  }
+  // componentWillEnter(cb) {
+  //   const el = this.container;
+  //   TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1, onComplete: cb});
+  // }
 
-  componentWillLeave(cb) {
+  // componentWillLeave(cb) {
+  //   const el = this.container;
+  //   TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0, onComplete: cb});
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
     const el = this.container;
-    TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0, onComplete: cb});
+    let prevShow = prevProps.show;
+    let currShow = this.props.show;
+    console.log(prevShow, currShow);
+    if (!prevShow && currShow) {
+      TweenMax.fromTo(el, 0.3, {y: 100, opacity: 0}, {y: 0, opacity: 1});
+    } 
+    if (prevShow && !currShow) {
+      TweenMax.fromTo(el, 0.3, {y: 0, opacity: 1}, {y: -100, opacity: 0});
+    }
   }
 
   render() {
     return (
       <div className={styles.container} ref={c => this.container = c}>
           <div className={styles.splash}>
-              <h1 className={styles.splashHead}>hello</h1>
+              <h1 className={styles.splashHead}>michaeltutt.io</h1>
               <p className={styles.splashSubhead}>
                 scroll down or just <a className={styles.resume} href="">view my resume</a>
               </p>
