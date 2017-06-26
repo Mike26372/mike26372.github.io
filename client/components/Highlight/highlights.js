8b67f479b75b0c032bab7a06428b63a82383b210
Michael Tutt <tutt.michael@gmail.com>
2017-06-26 15:34:08 -0700

    update highlight text for svg mapping

--- client/components/Highlight/highlights.js
+++ client/components/Highlight/highlights.js
@@ -1,8 +1,8 @@
 var highlights = [
-  {title: "Optimization Obsession", text: "Concise, semantic code is a way of life. Constantly utilizing modern front-end tools / best-practices to make user experience second to none", icon: null},
-  {title: "Creative Problem Solver", text: "Love for discovering new tools used to conquer unique problems, and I'll work tirelessly to solve them", icon: null},
-  {title: "JavaScript Ninja", text: "Significant experience with both modern front-end frameworks (React, Angular, etc.) with server-side MVC frameworks (Node / Express)", icon: null},
-  {title: "Design Focused", text: "Utilizing prime organization skills and substantial attention to detail, I strive to make sure every pixel is in order", icon: null},
+  {title: "Optimization Obsession", text: "Concise, semantic code is a way of life. Constantly utilizing modern front-end tools / best-practices to make user experience second to none", icon: 'Spedometer'},
+  {title: "Creative Problem Solver", text: "Love for discovering new tools used to conquer unique problems, and I'll work tirelessly to solve them", icon: 'Lightbulb'},
+  {title: "JavaScript Ninja", text: "Significant experience with both modern front-end frameworks (React, Angular, etc.) with server-side MVC frameworks (Node / Express)", icon: 'Computer'},
+  {title: "Design Focused", text: "Utilizing prime organization skills and substantial attention to detail, I strive to make sure every pixel is in order", icon: 'Design'},
 ];
 
 export default highlights; 
