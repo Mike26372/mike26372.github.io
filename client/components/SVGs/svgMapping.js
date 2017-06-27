e946312d985989e20d8c5bbf76252c2dc9f07e06
Michael Tutt <tutt.michael@gmail.com>
2017-06-27 11:35:11 -0700

    create new section for SVGs and add mapping

---
+++ client/components/SVGs/svgMapping.js
@@ -0,0 +1,17 @@
+
+import Lightbulb from './LightbulbSVG/LightbulbSVG.js';
+import Computer from './ComputerSVG/ComputerSVG.js';
+import Design from './DesignSVG/DesignSVG.js';
+import Spedometer from './SpedometerSVG/SpedometerSVG.js';
+import FullStack from './FullStackSVG/FullStackSVG.js';
+
+
+const svgMapping = {
+  'Computer': Computer,
+  'Spedometer': Spedometer,
+  'Design': Design,
+  'Lightbulb': Lightbulb,
+  'FullStack': FullStack
+};
+
+export default svgMapping;
