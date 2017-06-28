f052faf76423259bb50036f66311acf4abb25733
Michael Tutt <tutt.michael@gmail.com>
2017-06-27 21:25:12 -0700

    update svg map

--- client/components/SVGs/svgMapping.js
+++ client/components/SVGs/svgMapping.js
@@ -4,6 +4,11 @@ import Computer from './ComputerSVG/ComputerSVG.js';
 import Design from './DesignSVG/DesignSVG.js';
 import Spedometer from './SpedometerSVG/SpedometerSVG.js';
 import FullStack from './FullStackSVG/FullStackSVG.js';
+import FrontEnd from './FrontEndSVG/FrontEndSVG.js';
+import BackEnd from './BackEndSVG/BackEndSVG.js';
+import Language from './LanguageSVG/LanguageSVG.js';
+import Database from './DatabaseSVG/DatabaseSVG.js';
+import Tool from './ToolSVG/ToolSVG.js';
 
 
 const svgMapping = {
@@ -11,7 +16,12 @@ const svgMapping = {
   'Spedometer': Spedometer,
   'Design': Design,
   'Lightbulb': Lightbulb,
-  'FullStack': FullStack
+  'FullStack': FullStack,
+  'FrontEnd' : FrontEnd,
+  'BackEnd': BackEnd,
+  'Language': Language,
+  'Database': Database,
+  'Tool': Tool
 };
 
 export default svgMapping;
