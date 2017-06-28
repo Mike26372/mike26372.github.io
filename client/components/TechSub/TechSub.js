4d7969df43d8c45146f0b2380377917d6ddc5f65
Michael Tutt <tutt.michael@gmail.com>
2017-06-27 21:24:11 -0700

    add component to render each tech section

---
+++ client/components/TechSub/TechSub.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import styles from './TechSub.css';
+
+import svgMapping from '../SVGs/svgMapping.js';
+
+
+const TechSub = (props) => {
+
+  let row1Column = `pure-u-1 ${styles.row}`;
+  let row5Columns = `pure-u-1 pure-u-md-1-5 ${styles.row}`;
+  let { title, mainSkills, subSkills, icon } = props;
+
+  //Styles
+  let iconStyle = ``;
+  let titleStyle = `${styles.title}`;
+  let mainStyle = `${styles.main}`;
+  let subStyle = `${styles.sub}`;
+
+  return (
+    <div className={row5Columns}>
+      <div className={ iconStyle }>
+        {React.createElement(svgMapping[icon])}
+      </div>
+      <h3 className={ titleStyle }>
+        {title}  
+      </h3>
+      <div className={ mainStyle }>
+       {mainSkills.map(skill => <div>{skill}</div>)}
+      </div>    
+      <div className={ subStyle }>
+        {subSkills.map(sub => <div className={styles.subItem}>{sub}</div>)}
+      </div>    
+    </div>
+  )
+}
+
+export default TechSub;
