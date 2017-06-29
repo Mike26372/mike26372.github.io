42855621c28d32122612fa92216b5ad2a5d4875c
Michael Tutt <tutt.michael@gmail.com>
2017-06-28 21:42:28 -0700

    update highlights for new improved text

--- client/components/Highlight/highlights.js
+++ client/components/Highlight/highlights.js
@@ -1,8 +1,23 @@
 var highlights = [
-  {title: "Optimization Obsession", text: "Concise, semantic and modular code is not merely a best practice, but a way of life ", icon: 'Spedometer'},
-  {title: "Creative Problem Solver", text: "Ability to conceptualize an intricate paradigm comes naturally to me; the more complex, the more exciting", icon: 'Lightbulb'},
-  {title: "Expeditious Learner", text: "Excited by the opportunity to learn new tools and can get up to speed quickly", icon: 'Brain'},
-  {title: "Design Focused", text: "Attention to detail second to none", icon: 'Design'},
+  {
+    title: "Optimization Obsession", 
+    text: "Concise, semantic and modular code is not merely a best practice, but a way of life ", 
+    icon: 'Spedometer'
+  },
+  {
+    title: "Creative Problem Solver", 
+    text: "Ability to conceptualize an intricate paradigm comes naturally to me; the more complex, the more exciting", 
+    icon: 'Lightbulb'
+  },
+  {
+    title: "Expeditious Learner", 
+    text: "Excited by the opportunity to learn new tools and can get up to speed quickly", icon: 'Brain'
+  },
+  {
+    title: "Design Focused", 
+    text: "Attention to detail second to none", 
+    icon: 'Design'
+  },
 ];
 
 export default highlights; 
