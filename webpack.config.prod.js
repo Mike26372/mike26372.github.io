b635bcbee2564fccc8c62be84710629681836f3b
Michael Tutt <tutt.michael@gmail.com>
2017-06-28 21:44:29 -0700

    update webpack configuration for fontawesome

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -50,6 +50,15 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      },
+      // Font-Awesome
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: 'file-loader' 
       }
     ]
   }
