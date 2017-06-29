89d79f052ad67404dba45709698f3fd0a5e833fa
Michael Tutt <tutt.michael@gmail.com>
2017-06-29 15:29:52 -0700

    change use of autoprefixer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,6 +4,9 @@ var postCSSConfig = require('./postcss.config.js');
 var CompressionPlugin = require('compression-webpack-plugin');
 var postcssImport = require('postcss-import');
 var autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const postcssCssnext = require('postcss-cssnext');
+
 
 module.exports = {
   devtool: 'source-map',
@@ -22,7 +25,7 @@ module.exports = {
     new webpack.LoaderOptionsPlugin({
       test: /\.css$/,
       options: {
-        postcss: function() {
+        postcss: () => {
           return postCSSConfig;
         }
       }
@@ -33,7 +36,9 @@ module.exports = {
       test: /\.js$|\.css$|\.html$/,
       threshold: 0,
       minRatio: 0.8
-    })
+    }),
+    // new ExtractTextPlugin('application.css')
+
   ],
   module: {
     loaders: [
@@ -46,7 +51,23 @@ module.exports = {
       // CSS
       {
         test: /\.css$/,
+        // use: [
+        //   'style-loader',
+        //   { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+        //   { 
+        //     loader: 'postcss-loader', 
+        //     options: {
+        //       plugins: (loader) => [
+        //         require('postcss-import')({ root: loader.resourcePath }),
+        //         // require('postcss-cssnext')(),
+        //         require('autoprefixer')(),
+        //         // require('cssnano')()
+        //       ]
+        //     } 
+        //   }
+        // ]
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]---[local]---[hash:base64:5]!postcss-loader',
+        // loader: ExtractTextPlugin.extract(['style-loader', 'css-loader?modules&importLoaders=1', 'postcss-loader'])
       },
       // Font-Awesome
       { 
@@ -58,6 +79,9 @@ module.exports = {
         loader: 'file-loader' 
       }
     ]
-  }
+  },
+  // postcss: function() {
+  //   return postCSSConfig;
+  // },
 };
 
