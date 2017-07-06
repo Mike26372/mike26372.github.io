83efa535c1214f40f9876b1e170677f5a1473233
Michael Tutt <tutt.michael@gmail.com>
2017-07-06 13:51:22 -0700

    add colorpicker component for testing

--- client/components/ColorPicker/ColorPicker.js
+++ client/components/ColorPicker/ColorPicker.js
@@ -21,21 +21,28 @@ class ColorPicker extends Component {
   }
 
   handleColor(e) {
-    // console.log(e.target.value);
+    console.log(e.target.value);
     document.documentElement.style.setProperty(`--base`, e.target.value);
+    this.input.setProperty('value', e.target.value);
   }
 
   render() {
 
     let colorContainer = `${styles.colorPicker}`;
+    let inputStyle = `${styles.colorInput}`;
+    let labelStyle = `${styles.colorLabel} fa fa-paint-brush`;
 
     return (
       <div className={ colorContainer }>
         <div>
           <input ref={ (i) => this.input = i } 
             id="base" type="color" name="base" 
-            value="#262626"
-            onChange={this.handleColor} />
+            // value="#262626"
+            onChange={this.handleColor} 
+            className={inputStyle}/>
+          <label htmlFor="base" 
+          onChange={this.handleColor}
+          className={labelStyle}></label>
         </div>
       </div>
     )
